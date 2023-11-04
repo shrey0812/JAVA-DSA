49be016735b58c9e419c3450404f6c2508ae329c
shrey0812 <shreygupta0812@gmail.com>
2023-11-04 11:09:03 +0530

    initial commit

---
+++ array/DuplicateNumber.java
@@ -0,0 +1,31 @@
+package array;
+
+import java.util.Scanner;
+
+public class DuplicateNumber {
+    public static void main(String args[]){
+        int n;int count =0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        n = sc.nextInt();
+        int[] arr= new int[n];
+        System.out.println("Enter the elements of an array");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+            if(count>1)
+            {
+                System.out.println("the number repeating itself is "+ arr[i]);
+            }
+        }
+    }
+}
---
+++ array/FindNumber.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class FindNumber{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[100];
+        int c=1;
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            arr[i]=c;
+            c++;
+        }
+        System.out.println("Enter the missing number");
+        int a = sc.nextInt();
+        for(int i= 0;i<arr.length;i++){
+            if(arr[i]==a){
+                index = i;
+                break;
+            }
+        }
+        if(index!=0){
+            System.out.println("the missing number is at location "+ index);
+        }
+        else{
+            System.out.println("not found");
+        }
+    }
+}
