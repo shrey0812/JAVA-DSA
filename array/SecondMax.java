a9b1bd0dd69d30fc3766d92254280c26aaafadaa
shrey0812 <shreygupta0812@gmail.com>
2023-11-04 12:00:30 +0530

    commit

--- array/SecondMax.java
+++ array/SecondMax.java
@@ -2,10 +2,12 @@ package array;
 
 import java.util.Scanner;
 
-public class MinimumValue {
+public class SecondMax {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int n;
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
         n = sc.nextInt();
         int[] arr= new int[n];
@@ -13,14 +15,16 @@ public class MinimumValue {
         for(int i=0;i<n;i++){
             arr[i]= sc.nextInt();
         }
-        int low=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            if(arr[i]<low){
-                low = arr[i];
+            if(arr[i]>max){
+                secondMax=max;
+                max = arr[i];
             }
+            else if(arr[i]>secondMax&&arr[i]!=max){
+                secondMax= arr[i];
             }
-            System.out.println("The minimum value is "+low);
-        sc.close();
         }
+        System.out.println("The second maximum number is "+secondMax);
+        sc.close();
     }
-
+}
