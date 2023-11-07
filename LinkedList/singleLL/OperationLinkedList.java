89fdbe319c730ac8e9129fb8398e47c2b0aab480
shrey0812 <shreygupta0812@gmail.com>
2023-11-07 09:47:44 +0530

    initial commit

--- LinkedList/singleLL/OperationLinkedList.java
+++ LinkedList/singleLL/OperationLinkedList.java
@@ -205,6 +205,24 @@ public class OperationLinkedList {
     }
 
 
+    public void insertSorted(int value){
+        ListNode newNode= new ListNode(value);
+        ListNode current=head;
+        ListNode temp=null;
+        if(head==null){
+            return;
+        }
+        else{
+            while (current!=null&&current.data<newNode.data) {
+                temp=current;
+                current=current.next;
+            }
+            temp.next=newNode;
+            newNode.next=current;
+        }
+    }
+
+
     public static void main(String[] args) {
         OperationLinkedList oll =new OperationLinkedList();
         Scanner sc = new Scanner(System.in);
@@ -238,7 +256,9 @@ public class OperationLinkedList {
         // System.out.println("The middlenode is "+middleNode);
         // ListNode node = oll.findNodeFromEnd(5);
         // System.out.println(node.data);
-         oll.removeDupFromSortedLL();
+        // oll.removeDupFromSortedLL();
+        // oll.display();
+        oll.insertSorted(4);
         oll.display();
         sc.close();
     }
