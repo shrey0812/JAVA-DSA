6d4ca74181aed0d6366ac7cc0e8fe618587d0782
shrey0812 <shreygupta0812@gmail.com>
2023-11-16 13:59:45 +0530

    commit

--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -29,4 +29,15 @@ public class BinaryTree {
         second.left=fourth;
         second.right=fifth;
     }
+
+
+    public void preOrder(TreeNode root){
+        if (root==null) {
+            return;
+        }
+
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
 }
