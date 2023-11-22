dcb00e61969bd2f7f19db61c727add1522cb3d0d
shrey0812 <shreygupta0812@gmail.com>
2023-11-22 12:41:20 +0530

    commit

--- tree/BinarySearchTree.java
+++ tree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Scanner;
+
 public class BinarySearchTree {
     public TreeNode root;
 
@@ -11,6 +13,10 @@ public class BinarySearchTree {
         public TreeNode(int data){
             this.data=data;
         }
+    }
+
+    public void insertRecursive(int value){
+        root= insertRecursive(root,value);
     }
 
 
@@ -26,5 +32,46 @@ public class BinarySearchTree {
             root.right=insertRecursive(root.right, value);
         }
         return root;
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+
+    public void inOrder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    public TreeNode search(TreeNode root,int value){
+        if(root==null||root.data==value){
+            return root;
+        }
+        if(root.data>value){
+            return search(root.left, value);
+        }
+        else{
+            return search(root.right, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bs = new BinarySearchTree();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of nodes in the tree");
+        int n = sc.nextInt();
+        int value;
+        System.out.println("Enter the value of nodes");
+        for (int i = 0; i < n; i++) {
+            value = sc.nextInt();
+            bs.insertRecursive(value);
+        }
+        bs.inOrder();
+        sc.close();
     }
 }
