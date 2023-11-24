7f65c3cfc1f91e53fd55d0d451657006b95a910d
shrey0812 <shreygupta0812@gmail.com>
2023-11-24 20:09:13 +0530

    commit

--- heap/BinaryHeap/MaxPq.java
+++ heap/BinaryHeap/MaxPq.java
@@ -1,8 +1,10 @@
 package heap.BinaryHeap;
 
+import java.util.Scanner;
+
 public class MaxPq {
-    Integer[] heap;
-    int n;
+    private Integer[] heap;
+    private int n;
 
     public MaxPq(int capacity){
         heap = new Integer[capacity+1];
@@ -17,9 +19,25 @@ public class MaxPq {
         return n;
     }
 
+    public void insert(int value){
+        n++;
+        heap[n] = value;
+    }
+
     public static void main(String[] args) {
-        MaxPq maxPq = new MaxPq(3);
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the capacity of the heap");
+        int capacity = sc.nextInt();
+        MaxPq maxPq = new MaxPq(capacity);
+        int value;
+        System.out.println("Enter the components of the heap");
+        for (int i = 0; i < capacity; i++) {
+            value = sc.nextInt();
+            maxPq.insert(value);
+        }
         System.out.println(maxPq.size());
         System.out.println(maxPq.isEmpty());
+        sc.close();
     }
 }
