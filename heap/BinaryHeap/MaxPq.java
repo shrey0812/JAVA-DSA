9876331f94555fc5b4d2743e6e10cb064c37a55b
shrey0812 <shreygupta0812@gmail.com>
2023-11-28 18:43:30 +0530

    commit

--- heap/BinaryHeap/MaxPq.java
+++ heap/BinaryHeap/MaxPq.java
@@ -35,28 +35,28 @@ public class MaxPq {
     }
 
     
-    // public void display(){
-    //     System.out.println("The elements of heap are");
-    //     for (int i = 1; i < heap.length; i++) {
-    //         System.out.print(heap[i]+" ");
-    //     }
-    // }
+    public void display(){
+        System.out.println("The elements of heap are");
+        for (int i = 1; i < heap.length; i++) {
+            System.out.print(heap[i]+" ");
+        }
+    }
 
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        // System.out.println("enter the capacity of the heap");
-        // int capacity = sc.nextInt();
-        // MaxPq maxPq = new MaxPq(capacity);
-        // int value;
-        //System.out.println("Enter the components of the heap");
-        // for (int i = 0; i < capacity; i++) {
-        //     value = sc.nextInt();
-        //     maxPq.insert(value);
-        // }
-        // System.out.println(maxPq.size());
-        // System.out.println(maxPq.isEmpty());
-        // maxPq.display();
+        System.out.println("enter the capacity of the heap");
+        int capacity = sc.nextInt();
+        MaxPq maxPq = new MaxPq(capacity);
+        int value;
+        System.out.println("Enter the components of the heap");
+        for (int i = 0; i < capacity; i++) {
+            value = sc.nextInt();
+            maxPq.insert(value);
+        }
+        System.out.println(maxPq.size());
+        System.out.println(maxPq.isEmpty());
+        maxPq.display();
         sc.close();
     }
 }
